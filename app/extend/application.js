7958aced39ef2787c2d48337c2af61f1dcd28c6c
xiang <1984871009@qq.com>
2023-02-08 16:21:16 +0800

    feat: 修改

--- app/extend/application.js
+++ app/extend/application.js
@@ -1,12 +1,18 @@
 'use strict';
 
 const SRRouter = require('../../lib/routes');
+const path = require('path');
 
 const ROUTE_DEFINE = Symbol('Application#routeDefine');
 const ACCESS = Symbol('Application#Access');
 const ERROR = Symbol('Application#Error');
 
 module.exports = {
+  srLoadRoutes: outApp => new this.loader.FileLoader({
+    directory: path.join(this.config.baseDir, this.config.superRouter.routesPath || 'app/routes'),
+    target: this.routes,
+    inject: outApp,
+  }).load(),
   get srRouter() {
     if (!this[ROUTE_DEFINE]) {
       this[ROUTE_DEFINE] = new SRRouter(this);
