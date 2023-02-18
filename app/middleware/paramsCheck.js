8e5b784185275b297050015f365321ebf191cb46
xiang <1984871009@qq.com>
2023-02-18 14:23:57 +0800

    feat: 参数不允许未知参数

--- app/middleware/paramsCheck.js
+++ app/middleware/paramsCheck.js
@@ -6,6 +6,10 @@ const { EnumError } = require('../../lib/error');
 module.exports = () => {
   return async function paramsCheck(ctx, next) {
     const { routeInfo, app } = ctx;
+    let allowUnknow = true;
+    if (app && app.config && app.config.superRouter && app.superRouter.params) {
+      allowUnknow = app.superRouter.params.allowUnknow;
+    }
     try {
       const { params, method } = routeInfo;
       for (const field in params) {
@@ -14,7 +18,7 @@ module.exports = () => {
           ctx.urlParams[field] = ctx.queries[field];
         }
       }
-      ctx.urlParams = await joi.object(routeInfo.params).unknown().validateAsync(ctx.urlParams);
+      ctx.urlParams = await joi.object(routeInfo.params).unknown(allowUnknow).validateAsync(ctx.urlParams);
     } catch (err) {
       ctx.logger.error('params check failed, err = ', err);
       ctx.wrap(err.message, EnumError.ERR_PARAMS);
